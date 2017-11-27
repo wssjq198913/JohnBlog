4d808249cbdede4c92e8e6eba317bcc77a20755a
Johnny <>
2017-11-27 23:37:46 +0800

    add MD support

--- server/apiRoutes/blogDetail.js
+++ server/apiRoutes/blogDetail.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import fs from 'fs';
+import path from 'path';
 let blogDetailRoute = express.Router();
 
-const content = fs.readFileSync('Blogs/2017/06/01/test_blog/test_blog.md', 'utf-8');
+const content = fs.readFileSync(path.join(process.cwd(), 'Blogs/2017/06/01/test_blog/test_blog.md'), 'utf-8');
+console.log(path.join(process.cwd(), 'Blogs/2017/06/01/test_blog/test_blog.md'));
 
 blogDetailRoute.get('/loadBlogDetail', (req, res) => {
    res.json({
