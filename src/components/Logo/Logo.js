61fc9ad8a07f5839b49ca02d054806e6a4392c93
Johnny <>
2017-12-03 16:06:22 +0800

    UI updates

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -12,15 +12,27 @@ export default class Logo extends Component {
             <div className={`${styles['logo']} row`}>
                 <div className='col-md-8'>
                     <div className={`${styles['logo-left']}`}>
-                        <header></header>
+                        <header>
+                            <div>NO PAIN NO GAIN</div>
+                        </header>
                         <footer>
-                            <img src={head}/>
+                            <img src={head} />
                             <div>Johnny</div>
                         </footer>
                     </div>
                 </div>
                 <div className='col-md-4'>
-                    <div className={`${styles['logo-right']}`}></div>
+                    <div className={`${styles['logo-right']}`}>
+                        <div>
+                            <button className={styles.search + ' mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored'}>
+                                <i className="material-icons">search</i>
+                            </button>
+                        </div>
+                        <div>
+                            <div className={styles['head-icon']}></div>
+                        </div>
+                        <div>Johnny's Blog</div>
+                    </div>
                 </div>
             </div>
         )
