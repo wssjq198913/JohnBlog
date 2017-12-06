91b110bb7b771daa563c86a87b08a9eb6edf1d41
johnny.shi <johnny.shi@perficient.com>
2017-12-06 16:39:15 +0800

    set progress for the progress bar

--- src/components/Playbar/Playbar.js
+++ src/components/Playbar/Playbar.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
 import classNames from 'classnames/bind';
+import $ from 'jquery';
 import styles from './Playbar.scss';
 let cx = classNames.bind(styles);
-
+let tag = false;
+let left = 0;
 export default class Playbar extends Component {
     constructor(...props) {
         super(...props);
-        this.state = { locked: true };
+        this.state = { locked: true, currentTime: 0};
+    }
+    componentDidMount() {
+        $(document).on('mouseup', (event) => {
+            tag = false;
+            event.stopPropagation();
+        })
+        $(document).on('mousemove', (event) => {
+            if (tag) {
+                let originalPageX = $(`.${styles['play-progress']}`).offset().left;
+                left = event.pageX - originalPageX;
+                if (left <= 0) {
+                    left = 0;
+                }else if (left > 490) {
+                    left = 490;
+                }
+                $(`.${styles.dot}`).css('left', left);
+                event.stopPropagation();
+            }
+        })
     }
     lockPlaybar() {
         this.setState({ locked: !this.state.locked });
+    }
+    progressMoveStart(event) {
+        tag = true;
+        event.stopPropagation();
+    }
+    clickProgressBar(event) {
+        let originalPageX = $(`.${styles['play-progress']}`).offset().left;
+        left = event.pageX - originalPageX;
+        $(`.${styles.dot}`).css('left', left);
+        event.stopPropagation();
     }
     render() {
         let lockClass = cx({
@@ -35,8 +66,8 @@ export default class Playbar extends Component {
                                 <span className={styles.title}>我要快乐</span>
                                 <span className={styles.singer}>张惠妹</span>
                             </div>
-                            <div className={styles.progress}>
-                                <span className={styles.dot}></span>
+                            <div onClick={(e) => this.clickProgressBar(e)} className={styles.progress}>
+                                <span onMouseDown={(e)=>this.progressMoveStart(e)} className={styles.dot}></span>
                             </div>
                             <div className={styles.time}></div>
                         </div>
